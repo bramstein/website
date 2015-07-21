647deaea3b5aa1b06cdb924c1ac78e72295d85a8
Bram Stein <stein@adobe.com>
2015-07-22 01:07:31 +0200

    Add GA.

--- src/assets/js/typekit.js
+++ src/assets/js/typekit.js
@@ -8,4 +8,17 @@
     }, 0);
   };
   document.head.appendChild(script);
+
+  window['_gaq'] = window['_gaq'] || [];
+  window['_gaq'].push(['_setAccount', 'UA-10076742-1']);
+  window['_gaq'].push(['_trackPageview']);
+
+  var ga = document.createElement('script');
+
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
 }());
