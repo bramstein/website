57edddfb62714889807313e1ccb548bccc972c25
Bram Stein <b.l.stein@gmail.com>
2016-04-07 11:51:30 +0200

    Prevent micro-FOUT when the fonts are in the cache. Replace it with micro-FOIT.

--- src/assets/js/ga.js
+++ src/assets/js/ga.js
@@ -1,15 +1,4 @@
 (function () {
-  var typekit = document.createElement('script');
-  typekit.src = 'https://use.typekit.net/sta3bzx.js';
-  typekit.async = true;
-  typekit.onload = function () {
-    setTimeout(function () {
-      Typekit.load();
-    }, 0);
-  };
-
-  document.head.appendChild(typekit);
-
   window['_gaq'] = window['_gaq'] || [];
   window['_gaq'].push(['_setAccount', 'UA-10076742-1']);
   window['_gaq'].push(['_trackPageview']);
